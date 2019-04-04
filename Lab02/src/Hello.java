044a8f9277ff68e29445cce004a96e5bac72a5b3
Jaspreet Singh <Jaspreet Singh@DESKTOP-ROLADUF.attlocal.net>
2019-04-04 13:57:32 -0700

    Added a dot after my name.

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -7,7 +7,7 @@ public class Hello {
 		// Add a new line with your information
 		System.out.println("219467431 - This line was added by Ong Thao.");
 		System.out.println("219602618 - This line was added by Jericho Rivero.");
-		System.out.println("219859927 - This line was added by Jaspreet Singh");
+		System.out.println("219859927 - This line was added by Jaspreet Singh.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
